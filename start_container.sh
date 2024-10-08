ab1496421276b184458be2c398bc3378ac96daad
kotresha <kotreshabca@gmail.com>
2024-10-08 06:29:30 +0000

    second commit

--- start_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull kotreshad/python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 8080:8080 kotreshad/python-flask-app
